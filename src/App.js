ff16351fe13af0109a816ceccea8d394ea8d613e
Todd Myler <tmyler@bandwidth.com>
2021-02-22 16:36:34 -0500

    Open Issue on Failure of Pull Request

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn
           <p>Hello World</p>
         </a>
       </header>
