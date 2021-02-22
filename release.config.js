3ef22604db5669688aaaf3799acdb7d727d89379
Todd Myler <tmyler@bandwidth.com>
2021-02-22 15:20:07 -0500

    Update json

--- release.config.js
+++ release.config.js
@@ -5,13 +5,13 @@ module.exports = {
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     [
-        "@semantic-release/github", 
-        {
-          assets: [
-            { path: "build.zip", label: "Build"},
-            { path: "coverage.zip", label: "Coverage"}
-          ]
-       }
-    ]
-  ]
+      "@semantic-release/github",
+      {
+        assets: [
+          { path: "build.zip", label: "Build" },
+          { path: "coverage.zip", label: "Coverage" },
+        ],
+      },
+    ],
+  ],
 };
