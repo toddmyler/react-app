aa344487628b5e92bf8f35cc2ba88418a014e7fc
Todd Myler <tmyler@bandwidth.com>
2021-02-22 13:54:15 -0500

    Format js Code

--- release.config.js
+++ release.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-    branches: "master",
-    repositoryUrl: "https://github.com/toddmyler/react-app",
-    plugins: [
-        "@semantic-release/commit-analyzer",
-        "@semantic-release/release-notes-generator",
-        "@semantic-release/github"]
-}
+  branches: "master",
+  repositoryUrl: "https://github.com/toddmyler/react-app",
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/github",
+  ],
+};
