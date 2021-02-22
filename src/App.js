15495e4cc3353c9b1f92f62ca32ffd3f9ca9a5fc
Todd Myler <tmyler@bandwidth.com>
2021-02-22 16:49:19 -0500

    fix: add README.md test badge

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn
+          Learn React
           <p>Hello World</p>
         </a>
       </header>
